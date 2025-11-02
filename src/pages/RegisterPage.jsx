5df653d790c5a6f8181f98aa9f99913070632c45
rimpal <thakorrimpal30@gmail.com>
2025-11-02 12:47:12 +0530

    Fix missing Button component path

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react"
 import { useNavigate, Link } from "react-router-dom"
-import { Button } from "../components/ui/button"
+import { Button } from "../components/ui/Button"
 import {
   Card,
   CardContent,
